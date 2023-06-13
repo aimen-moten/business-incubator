898c925e92f6886727b38aef628d21028824729b
Aimen Moten <aimen.moten@gmail.com>
2023-06-13 21:27:01 +0500

    about,contact,programs

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,7 +1,34 @@
 import React from 'react'
+import { biHero } from '../assets'
+import Footer from './Footer'
 
 export default function AboutUs() {
   return (
-    <div>AboutUs</div>
+    <div className='bg-slate-200 pt-24 mx-auto max-w-full h-full' >
+      <h1 className='text-7xl text-gray-900 font-bold text-center pb-12'>Who are we?</h1>
+      <div className='flow root'>
+          <h4 className='mx-20 text-[18px] pb-20'>
+            Introducing <b>Infinity Business Incubator</b>: Elevate Your Entrepreneurial Potential
+            <br/>
+            <br/>
+            Are you ready to unleash the infinite possibilities of your business idea? Welcome to Infinity Business Incubator, the groundbreaking web app designed to empower aspiring entrepreneurs with mentorship, exclusive resources, signature programs, and so much more.
+            Infinity Business Incubator is your gateway to realizing your entrepreneurial dreams. Connect with seasoned mentors, access a wealth of exclusive resources, and enroll in signature programs tailor-made to accelerate your business journey.
+            <br/>
+            <br/>
+            Fuel your ambition by joining our mentorship program, where industry experts and successful entrepreneurs provide invaluable guidance, support, and practical advice. Benefit from their wisdom, avoid common pitfalls, and gain the confidence to navigate the business landscape with finesse.
+            Experience the power of limitless resources through Infinity Business Incubator. Our curated library offers an extensive array of exclusive tools, from comprehensive business templates to market research reports, financial planning guides, and cutting-edge marketing strategies. Harness the knowledge that will help you strategize, innovate, and conquer the market.
+            <br/>
+            <br/>
+            Elevate your potential with our signature programs, designed to cater to entrepreneurs at every stage of their journey. Whether you're refining your business plan, seeking funding opportunities, or scaling up your operations, our programs provide actionable insights, hands-on workshops, and networking opportunities with industry leaders.
+            Infinity Business Incubator fosters a vibrant community of like-minded individuals who understand that entrepreneurship thrives in collaboration. Engage in thought-provoking discussions, participate in interactive webinars, and forge meaningful connections that will support your success.
+            <br/>
+            <br/>
+            It's time to transcend limits and unlock the infinite possibilities of your business. Join Infinity Business Incubator today and elevate your entrepreneurial potential with unparalleled mentorship, exclusive resources, signature programs, and a world of possibilities. Get ready to ignite your entrepreneurial journey like never before.
+          </h4>
+
+          <img src={biHero} className='' ></img>
+      </div>
+      <Footer/>
+    </div>
   )
 }
--- src/components/ProgramOverview.js
+++ src/components/ProgramOverview.js
@@ -1,7 +1,41 @@
 import React from 'react'
+import Footer from './Footer'
 
 export default function ProgramOverview() {
   return (
-    <div>ProgramOverview</div>
+    <div className='bg-slate-200'>
+      <h1 className='text-gray-900 font-bold text-7xl text-center py-24'>Our Programs</h1>
+      
+
+
+      <div className='bg-gray-900 flow-root px-20'>
+        <h1 className='text-slate-200 font-bold text-6xl py-24 float-left max-w-[50%] '>Startup Accelerator Bootcamp
+       
+        <br/>
+        <span className='text-[24px] text-white font-normal'>Program Cost: $500</span>
+        </h1>
+        <h2 className='text-[18px] text-slate-200 py-24'>The Startup Accelerator Bootcamp is an intensive 12-week program that equips aspiring entrepreneurs with the skills, knowledge, and network they need to launch successful startup ventures. Participants will learn from experienced industry mentors and receive hands-on training in key areas such as business model development, market research, funding strategies, and investor pitch preparation. Through a combination of workshops, seminars, and networking events, this program aims to accelerate the growth of early-stage startups and help entrepreneurs navigate the challenges of the startup ecosystem.</h2>
+      </div>
+
+      <div className='bg-slate-200 flow-root px-20'>
+        <h1 className='text-gray-900 font-bold text-6xl py-24 float-left max-w-[50%] '>Tech Innovators Fellowship
+       
+        <br/>
+        <span className='text-[24px] text-gray-700 font-normal'>Program Cost: $1000</span>
+        </h1>
+        <h2 className='text-[18px] text-gray-700 py-24'> The Tech Innovators Fellowship is a year-long program designed for tech-savvy entrepreneurs who are looking to leverage emerging technologies to drive innovation in their startup ventures. This highly selective fellowship provides participants with access to cutting-edge resources, including mentorship from industry experts, technical workshops, and state-of-the-art prototyping facilities. Over the course of the program, fellows will receive guidance on areas such as artificial intelligence, blockchain, Internet of Things (IoT), and cybersecurity, enabling them to develop groundbreaking solutions and gain a competitive edge in the startup landscape.</h2>
+      </div>
+
+      <div className='bg-gray-900 flow-root px-20'>
+        <h1 className='text-slate-200 font-bold text-6xl py-24 float-left max-w-[50%] '>Social Impact Startup Lab
+       
+        <br/>
+        <span className='text-[24px] text-white font-normal'>Program Cost: $750</span>
+        </h1>
+        <h2 className='text-[18px] text-slate-200 py-24'>The Social Impact Startup Lab is a unique program dedicated to supporting entrepreneurs who are passionate about creating positive social change through their startup ventures. This intensive six-month lab offers a comprehensive curriculum that covers topics such as social entrepreneurship, impact measurement, sustainable business models, and community engagement strategies. Participants will work closely with industry experts, impact investors, and successful social entrepreneurs, gaining invaluable insights and guidance to scale their startups while making a meaningful difference in society. By fostering a collaborative and supportive environment, the lab empowers entrepreneurs to build sustainable businesses that address pressing social challenges.</h2>
+      </div>
+      
+      <Footer/>
+    </div>
   )
 }
