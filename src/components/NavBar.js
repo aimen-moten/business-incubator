6a42a5ebc3c21d9111dae1b877e9a2398a6e9679
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-06-13 09:49:43 +0530

    updated nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Navbar = () => {
+  const [isMenuOpen, setMenuOpen] = React.useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
   return (
-    <header className="bg-blue-900 py-4 shadow">
+    <header className="bg-gray-900 py-4 shadow">
       <nav className="container mx-auto px-4 flex items-center justify-between">
         <a href="/" className="text-2xl font-bold text-white">Infinity Business Incubator</a>
         <ul className="flex space-x-4">
-          <li><a href="/" className="text-lg text-white hover:text-gray-300">Home</a></li>
-          <li><a href="/" className="text-lg text-white hover:text-gray-300">About</a></li>
-          <li><a href="/" className="text-lg text-white hover:text-gray-300">Services</a></li>
-          <li><a href="/" className="text-lg text-white hover:text-gray-300">Contact</a></li>
+          <li><a href="/" className="text-lg text-white hover:text-orange-300 transition duration-300 ease-in-out transform hover:scale-110">Home</a></li>
+          <li><a href="/" className="text-lg text-white hover:text-orange-300 transition duration-300 ease-in-out transform hover:scale-110">About</a></li>
+          <li><a href="/" className="text-lg text-white hover:text-orange-300 transition duration-300 ease-in-out transform hover:scale-110">Services</a></li>
+          <li><a href="/" className="text-lg text-white hover:text-orange-300 transition duration-300 ease-in-out transform hover:scale-110">Contact</a></li>
+          <li><a href="/" className="text-lg text-white hover:text-orange-300 transition duration-300 ease-in-out transform hover:scale-110">login</a></li>
         </ul>
       </nav>
     </header>
