f91853104f4753c870d6bac6cc6d3d2847476f09
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-06-13 11:46:57 +0530

    navbar

---
+++ src/components/FirebaseConfig.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function FirebaseConfig() {
+    const firebaseConfig = {
+        apiKey: "AIzaSyAxeyK4drkDBeNFqYAlRoIbhqZ1oz0d6v4",
+        authDomain: "business-incubator-f3262.firebaseapp.com",
+        projectId: "business-incubator-f3262",
+        storageBucket: "business-incubator-f3262.appspot.com",
+        messagingSenderId: "747636003067",
+        appId: "1:747636003067:web:407af76f689818b608e05e",
+        measurementId: "G-Q1ZL9QSGQN"
+      };
+    return firebaseConfig;
+}
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,53 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { FaHome, FaUserCircle, FaCreativeCommons, FaFeatherAlt, FaConnectdevelop } from 'react-icons/fa';
+import { getDatabase, ref, push, onValue } from "firebase/database";
+import { initializeApp } from "firebase/app";
+import 'firebase/auth';
+import FirebaseConfig from './FirebaseConfig';
 
 const Navbar = () => {
-  const [isMenuOpen, setMenuOpen] = React.useState(false);
+  const app = initializeApp(FirebaseConfig);
 
-  const toggleMenu = () => {
-    setMenuOpen(!isMenuOpen);
+  const handleSignOut = () => {
+    app.auth().signOut()
+      .then(() => {
+        // Handle successful sign out
+      })
+      .catch((error) => {
+        // Handle sign out error
+        console.log(error);
+      });
   };
   return (
     <header className="bg-gray-900 py-4 shadow">
       <nav className="container mx-auto px-4 flex items-center justify-between">
         <a href="/" className="text-2xl font-bold text-white">Infinity Business Incubator</a>
         <ul className="flex space-x-4">
-          <li><a href="/" className="text-lg text-white hover:text-orange-300 transition duration-300 ease-in-out transform hover:scale-110">Home</a></li>
-          <li><a href="/" className="text-lg text-white hover:text-orange-300 transition duration-300 ease-in-out transform hover:scale-110">About</a></li>
-          <li><a href="/" className="text-lg text-white hover:text-orange-300 transition duration-300 ease-in-out transform hover:scale-110">Services</a></li>
-          <li><a href="/" className="text-lg text-white hover:text-orange-300 transition duration-300 ease-in-out transform hover:scale-110">Contact</a></li>
-          <li><a href="/" className="text-lg text-white hover:text-orange-300 transition duration-300 ease-in-out transform hover:scale-110">login</a></li>
+          <li>
+            <a href="/" className="text-lg text-white flex items-center hover:text-orange-300 transition duration-300 ease-in-out transform hover:scale-110">
+              <FaHome className="nav-icon mr-1" /> Home
+            </a>
+          </li>
+          <li>
+            <a href="/" className="text-lg text-white flex items-center hover:text-orange-300 transition duration-300 ease-in-out transform hover:scale-110">
+              <FaFeatherAlt className="nav-icon mr-1" /> Programs
+            </a>
+          </li>
+          <li>
+            <a href="/" className="text-lg text-white flex items-center hover:text-orange-300 transition duration-300 ease-in-out transform hover:scale-110">
+              <FaCreativeCommons className="nav-icon mr-1" />Templates
+            </a>
+          </li>
+          <li>
+            <a href="/" className="text-lg text-white flex items-center hover:text-orange-300 transition duration-300 ease-in-out transform hover:scale-110">
+              <FaConnectdevelop className="nav-icon mr-1" />Connect
+            </a>
+          </li>
+          <li>
+            <button onClick={handleSignOut} className="text-lg text-white flex items-center hover:text-orange-300 transition duration-300 ease-in-out transform hover:scale-110">
+              <FaUserCircle className="nav-icon mr-1" />
+            </button>
+          </li>
         </ul>
       </nav>
     </header>
